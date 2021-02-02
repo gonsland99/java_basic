7e00d9db00c607e175c0bca13ff6ff6d34741c09
Home <Home@Home-PC>
2021-02-03 02:18:14 +0900

    if 삼항연산자

---
+++ java_basic/src/java_basic/If_ex.java
@@ -0,0 +1,10 @@
+package java_basic;
+
+public class If_ex {
+	public static void main(String[] args) {
+		int year = 10;
+		String man = "";
+		man = year>10?"동네토박이 입니다":"신입주민입니다.";
+		System.out.println("본인은 "+year+"년 살아온"+man);
+	}
+}
