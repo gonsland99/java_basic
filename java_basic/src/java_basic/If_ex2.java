d14724b0599ab0d1143f193fbe6b2244d876cfb6
Home <Home@Home-PC>
2021-02-03 02:28:07 +0900

    if 삼항

---
+++ java_basic/src/java_basic/If_ex2.java
@@ -0,0 +1,11 @@
+package java_basic;
+
+public class If_ex2 {
+	public static void main(String[] args) {
+		int point = 100;
+		String man = "";
+		man = point > 99 ? "vip 입니다" : "bronze 입니다.";
+		System.out.println("본인은 " + point + "년 살아온" + man);
+	}
+
+}
