0ff3a05704bf18cc1c51809f777450c74e3b5ee5
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-03-24 18:32:26 +0900

    이스케이프 문자(역슬러시)

--- java_basic/src/java_basic/Escape.java
+++ java_basic/src/java_basic/Escape.java
@@ -2,12 +2,13 @@ package java_basic;
 
 public class Escape {
 	public static void main(String[] args) {
-// 이스케이프 문자 '\'
-		char c = '\n';	//escape문자는 컴퓨터가 1개 문자로 인식
+		
+		// 이스케이프 문자(escape문자는 컴퓨터가 1개 문자로 인식)
+		char c = '\n';
 		System.out.println("이스케이프 문자 ");
 		System.out.print("\\n: 다음줄로\n");
 		System.out.println("\\t: \t탭");
-		System.out.println("\\: 문자사용 ");
-		System.out.println("\": 문자사용");
+		System.out.println("\\: \\문자사용 ");
+		System.out.println("\": \"문자사용");
 	}
 }
