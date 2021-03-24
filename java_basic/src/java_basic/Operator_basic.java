fb3cd497d05563e01290c0738f135f7ff0b441de
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-03-24 18:49:01 +0900

    명시적/묵시적 형변환

--- java_basic/src/java_basic/Operator_basic.java
+++ java_basic/src/java_basic/Operator_basic.java
@@ -2,8 +2,7 @@ package java_basic;
 
 public class Operator_basic {
 	public static void main(String[] args) {
-		int a = 10;
-		
+		int a = 10;		
 		int avg = a/3;
 //		float avg = a/3.0f;		//묵시적 형변환
 //		float avg = (float)(a/3.0);	//명시적 형변환
