8e14b02819735e9f5de470fb9c79da3440a3c15f
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-03-24 18:39:22 +0900

    향상된for문(forEach)

--- java_basic/src/java_basic/ForEach.java
+++ java_basic/src/java_basic/ForEach.java
@@ -2,12 +2,20 @@ package java_basic;
 
 public class ForEach {
 	public static void main(String[] args) {
-		int [] arr = {1,2,3,5,7,10};
-//		for(int i=0; i<arr.length; i++) {
-//			System.out.println((i+1)+".값: "+arr[i]);
-//		}
-		for(int num : arr) {			//foreach문 (향상된 for문)
-			System.out.println("값: "+num);
+		//향상된 for문(배열문에 사용)
+		int [][] arr = {{1,2},{3,5},{7,10}};
+		/*		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length; i++) {
+				System.out.println((i+1)+".값: "+arr[i]);
+			}
+		}
+		 */
+		for(int[] a : arr) {
+			for(int b : a) {
+				System.out.print(b+"\t");
+			}
+			System.out.println();
 		}
 	}
 }
