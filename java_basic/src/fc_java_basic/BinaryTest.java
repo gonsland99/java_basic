aff6a8959026c76a56eaef5b508f6966ecd977d9
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-05-03 22:57:13 +0900

    자바 2,8,16진법 표기

---
+++ java_basic/src/fc_java_basic/BinaryTest.java
@@ -0,0 +1,15 @@
+package fc_java_basic;
+
+public class BinaryTest {
+	public static void main(String[] args) {
+		int num = 10;
+		int bNum = 0b1010;	//2진수(0b~)
+		int oNum = 012;		//8진수(0~)
+		int xNum = 0xA;		//16진수(0x~)
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+	}
+}
