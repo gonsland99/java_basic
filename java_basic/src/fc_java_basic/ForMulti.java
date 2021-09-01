98b44d0f76140decdf7c2d074a743861c74dc199
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-09-01 18:38:07 +0900

    구구단 가로, 세로 출력

---
+++ java_basic/src/fc_java_basic/ForMulti.java
@@ -0,0 +1,16 @@
+package fc_java_basic;
+
+public class ForMulti {
+	public static void main(String[] args) {
+//		for(int i=2; i<=9; i++) {
+//			for(int j=1; j<=9; j++) {
+//				System.out.print(i+" x "+j+" = "+i*j+"\t");
+//			}System.out.println();
+//		}
+		for(int i=1; i<=9; i++) {
+			for(int j=2; j<=9; j++) {
+				System.out.print(j+" x "+i+" = "+i*j+"\t");
+			}System.out.println();
+		}
+	}
+}
